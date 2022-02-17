ba04e59cbc351e43bcc8a5429d31e01e52ca5a87
Mateus Amaral <mateus.almeida@dtidigital.com.br>
2022-02-17 17:18:09 -0300

    model de produto e mapeamento do mesmo

---
+++ ControleProdutoApi/ControleProduto.BLL/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControleProduto.BLL.Models
+{
+    public class Produto
+    {
+        public int IdProduto { get; set; }
+        public string NomeProduto { get; set; }
+        public decimal Preco { get; set; }
+        public decimal PrecoPromocao { get; set; }
+        public int QuantidadeEstoque { get; set; }
+    }
+}
---
+++ ControleProdutoApi/ControleProdutoApi/Mapeamento/ProdutoMap.cs
@@ -0,0 +1,35 @@
+﻿using ControleProduto.BLL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleProdutoApi.Mapeamento
+{
+    public class ProdutoMap : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(p => p.IdProduto);
+            builder.Property(p => p.NomeProduto).IsRequired(false).HasMaxLength(20);
+            builder.Property(p => p.Preco).IsRequired(false);
+            builder.Property(p => p.PrecoPromocao).IsRequired(false);
+            builder.Property(p => p.QuantidadeEstoque).IsRequired(false);
+
+            builder.HasData(
+                new Produto
+                {
+                    IdProduto = 1,
+                    NomeProduto = "nome do produto",
+                    Preco = 15,
+                    PrecoPromocao = 10,
+                    QuantidadeEstoque = 100
+
+                });
+
+            builder.ToTable("Produto");
+        }
+    }
+}
